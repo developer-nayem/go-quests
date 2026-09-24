2abdce740b82c7af52e1ec38c0903eb99f265539
Md Jannatul Nayem <nayem@paymid.com>
2026-09-24 09:00:41 +0600

    completed map exercises

--- quests/006.maps/maps.go
+++ quests/006.maps/maps.go
@@ -7,38 +7,58 @@ type Cache struct {
 func NewCache() *Cache {
 	// TODO: initialize the cache
 	// Read README.md for the instructions
-	return &Cache{}
+
+	return &Cache{
+		data: make(map[string]int),
+	}
 }
 
 func (c *Cache) Set(key string, value int) {
 	// TODO: implement
 	// Read README.md for the instructions
+	c.data[key] = value
 }
 
 func (c *Cache) Get(key string) (int, bool) {
 	// TODO: implement
 	// Read README.md for the instructions
+	item, exists := c.data[key]
+
+	if exists {
+		return item,exists
+	}
+
 	return 0, false
 }
 
 func (c *Cache) Delete(key string) {
 	// TODO: implement
 	// Read README.md for the instructions
+	delete(c.data,key)
 }
 
 func (c *Cache) Count() int {
 	// TODO: implement
 	// Read README.md for the instructions
-	return 0
+	return len(c.data)
 }
 
 func (c *Cache) AllKeys() []string {
 	// TODO: implement
 	// Read README.md for the instructions
-	return []string{}
+	allKeys := make([]string, 0, len(c.data))
+	for key := range c.data{
+		allKeys = append(allKeys, key)
+	}
+	return allKeys
 }
 
 func (c *Cache) RemoveBelow(limit int) {
 	// TODO: implement
 	// Read README.md for the instructions
+	for key := range c.data{
+		if c.data[key] < limit {
+			delete(c.data, key)
+		}
+	}
 }
