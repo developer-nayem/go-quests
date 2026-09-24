9c7ae4f0f6ed7736c71c4a9cc19f990b9a715ef8
Md Jannatul Nayem <nayem@paymid.com>
2026-09-24 08:39:08 +0600

    slice exercises

--- quests/005.slice/slice.go
+++ quests/005.slice/slice.go
@@ -1,7 +1,74 @@
 package slice
 
+// naive approach
+// func ProcessScores(scores []int) []int {
+// 	//TODO: implement
+// 	// Read README.md for the instructions
+
+// 	filterdResult := []int{}
+
+// 	for _, value := range scores {
+//       if value >= 0 && value  <= 100 {
+//           filterdResult = append(filterdResult,value)
+//       }
+//   	}
+
+//   	normalizedResult := []int{}
+
+//   	for _, value := range filterdResult{
+//   		if value < 40{
+//   			value = 40
+//   			normalizedResult = append(normalizedResult, value)
+//   		} else {
+//   			normalizedResult = append(normalizedResult, value)
+//   		}
+//   	}
+
+//   	bonusResult := []int{}
+
+//   	if(len(normalizedResult) > 5){
+//   		for _, value := range normalizedResult{
+//   			if(value + 5 <= 100){
+//   				bonusResult = append(bonusResult, value+5)
+//   			}else {
+//   				diff := 100 - value
+//   				value += diff
+//   				bonusResult = append(bonusResult, value)
+//   			}
+//   		}
+//   	}
+
+//   	if(len(bonusResult) == 0){
+//   		return normalizedResult
+//   	}else {
+//   		return bonusResult
+//   	}
+// }
+
+// optimized way 
+
 func ProcessScores(scores []int) []int {
-	//TODO: implement
-	// Read README.md for the instructions
-	return []int{}
+
+	result := make([]int, 0, len(scores))
+
+	for _, value := range scores{
+		if( value >= 0 && value <= 100){
+			if(value < 40){
+				value = 40
+			}
+			result = append(result, value)
+
+		}
+	}
+
+	if(len(result) > 5){
+		for i := range result {
+			result[i] += 5
+			if(result[i] > 100){
+				result[i] = 100
+			}
+
+		}
+	}
+	return result 
 }
