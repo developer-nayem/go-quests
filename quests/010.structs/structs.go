c4b4c3830c2be2ca48a5a6d0f77d97e6271244e8
Md Jannatul Nayem <nayem@paymid.com>
2026-09-25 23:30:36 +0600

    done struct exercises

--- quests/010.structs/structs.go
+++ quests/010.structs/structs.go
@@ -10,31 +10,43 @@ type User struct {
 func NewUser(id int, name, email string, age int) *User {
 	// TODO: implement
 	// Read README.md for the instructions
-	return nil
+
+	return &User{
+		ID : id,
+		Name: name,
+		Email: email,
+		Age: age,
+	}
 }
 
 func (u *User) IsAdult() bool {
 	// TODO: implement
 	// Read README.md for the instructions
+	if u.Age >= 18 {
+		return true
+	}
 	return false
 }
 
 func (u User) DisplayName() string {
 	// TODO: implement
 	// Read README.md for the instructions
-	return ""
+	return u.Name + " <" + u.Email + ">"
 }
 
 func (u *User) UpdateEmail(email string) {
 	// TODO: implement
 	// Read README.md for the instructions
+	u.Email = email
 }
 
 func (u *User) Birthday() {
 	// TODO: implement
 	// Read README.md for the instructions
+	u.Age += 1
 }
 
 func CloneUser(u User) User {
-	return User{}
+	newUser := u 
+	return newUser
 }
