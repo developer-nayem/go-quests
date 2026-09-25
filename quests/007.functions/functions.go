c77c84fa6c56fb449f3a556caa3d41f877fdb148
Md Jannatul Nayem <nayem@paymid.com>
2026-09-25 09:34:01 +0600

    function exercises

--- quests/007.functions/functions.go
+++ quests/007.functions/functions.go
@@ -1,17 +1,34 @@
 package functions
+import (
+	"errors"
+)
 
 // Divide returns a / b or an error if b == 0
 func Divide(a, b int) (int, error) {
 	// TODO: implement
 	// Read README.md for instructions on returning proper errors
-	return 0, nil
+	if b == 0{
+		return 0, errors.New("b cannot be zero")
+	} else {
+		return a/b, nil 
+	}
+	
 }
 
 // SumAll returns the sum of all provided integers
 func SumAll(nums ...int) int {
 	// TODO: implement
 	// Read README.md for instructions on handling variadic arguments
-	return 0
+	sum := 0
+	if len(nums) == 0{
+		return sum
+	} else {
+		for _,value := range nums{
+			sum += value
+		}
+	}
+	return sum 
+	
 }
 
 // MaxMin returns the max and min of all provided integers
@@ -19,12 +36,39 @@ func SumAll(nums ...int) int {
 func MaxMin(nums ...int) (int, int, error) {
 	// TODO: implement
 	// Read README.md for instructions on proper error handling
-	return 0, 0, nil
+	if len(nums) == 0{
+		return 0,0,errors.New("no parameters passed")
+	}
+
+	min := nums[0]
+	max := nums[0]
+
+	for _, value := range nums {
+		if value > max {
+			max = value
+		}
+		if value < min {
+			min = value
+		}
+	}
+	return max,min,nil
 }
 
 // ConcatAll joins all strings using the provided separator
 func ConcatAll(sep string, strs ...string) string {
 	// TODO: implement
 	// Read README.md for instructions on variadic strings
-	return ""
+
+	result := ""
+	lastIndex := len(strs) - 1 
+
+	for i := 0; i < len(strs); i++ {
+		if i == lastIndex {
+			result += strs[i]
+		} else {
+			result +=  strs[i] + sep 
+		}
+	}
+
+	return result
 }
