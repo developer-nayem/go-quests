462902f5785106df19c95bf2a28fb06730a05c81
Md Jannatul Nayem <nayem@paymid.com>
2026-09-25 10:15:58 +0600

    string exercise complete

--- quests/009.strings/strings.go
+++ quests/009.strings/strings.go
@@ -1,5 +1,9 @@
 package strings
 
+import (
+	"unicode/utf8"
+)
+
 type TextStats struct {
 	ByteLength int
 	RuneCount  int
@@ -8,29 +12,69 @@ type TextStats struct {
 func AnalyzeText(s string) TextStats {
 	// TODO: implement
 	// Read README.md for the instructions
-	return TextStats{}
+	byteLength := len(s)
+	runeCount := utf8.RuneCountInString(s)
+	return TextStats{
+		byteLength,
+		runeCount,
+	}
 }
 
 func RuneFrequencies(s string) map[rune]int {
 	// TODO: implement
 	// Read README.md for the instructions
-	return map[rune]int{}
+	myMap := make(map[rune]int)
+
+	for _,value := range s {
+		_,exists := myMap[value]
+		if !exists {
+			myMap[value] = 1
+		}else {
+			myMap[value] += 1
+		}
+	}
+	return myMap
 }
 
 func FirstRunePosition(s string, target rune) int {
 	// TODO: implement
 	// Read README.md for the instructions
+
+	for i, value := range s {
+		if value == target {
+			return i
+		}
+	}
 	return -1
 }
 
 func ExtractRunes(s string) []rune {
 	// TODO: implement
 	// Read README.md for the instructions
-	return []rune{}
+
+	result := []rune{}
+
+	for _, value := range s {
+		result = append(result, value)
+	}
+
+	return result 
+	
 }
 
 func HasOnlyASCII(s string) bool {
 	// TODO: implement
 	// Read README.md for the instructions
-	return false
+	if len(s) == 0 {
+		return true 
+	}
+
+	for _, value := range s {
+		if int(value) > 127 {
+			return false 
+		}
+	}
+
+
+	return true
 }
